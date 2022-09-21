42d03f46ef2ba76c68f6fd99d7af780b907a41ad
jonnyboi <jonathan.wright@networkrail.co.uk>
2022-09-21 08:18:27 +0100

    hour update, error check, update if different

--- BalanceMonitor.py
+++ BalanceMonitor.py
@@ -15,9 +15,12 @@ from openpyxl.utils import get_column_letter
 
 
 def getBal(addr,client):
-        addrBalance = int(client.get_balance(addr)['result']['value'])/1000000000
-        print(addr," : ",addrBalance)
-        return addrBalance
+        try:
+                addrBalance = int(client.get_balance(addr)['result']['value'])/1000000000
+                print(addr," : ",addrBalance)
+                return addrBalance
+        except:
+                return 0
         
 
 
@@ -25,30 +28,36 @@ Daypre = 99
 
 while True:
         sleep(10)
-        day = strftime("%d", gmtime())
+        #day = strftime("%d", gmtime())#day check
+        day = strftime("%H", gmtime())#hour check
         if(day != Daypre):#once an day we check if we need to update
-            Daypre = day
-            if(client.is_connected()): 
+            if(client.is_connected()):
+                Daypre = day
                 wb = load_workbook('Wallet_balances.xlsx')
                 ws = wb.active
                 
-            
-                for row1 in ws["1"]:
+                for row1 in ws["1"]:#get first blank col for date and balances
                     ROW1 = row1
                 colDate = "%s1" % (get_column_letter(ROW1.column + 1))
-                #print(colDate)
-                ws[colDate] = datetime.datetime.now()
-                #ws[colDate].number_format
                            
                 colA = ws['A']      
-                for addr in colA:
+                for addr in colA:#get address in col A
                     #print(addr.value)
                     if(addr.value != "Wallet address"):
+                        
                         addrBalance = getBal(addr.value,client)
-                        colval = "%s%s" % (get_column_letter(ROW1.column + 1),addr.row)
-                        ws[colval] = addrBalance
+                        oldcolval = "%s%s" % (get_column_letter(ROW1.column),addr.row)
+                        #print("old value = ",ws[oldcolval].value)
+                        if(ws[oldcolval].value != addrBalance):
+                                ws.column_dimensions[get_column_letter(ROW1.column + 1)].width = 17
+                                ws[colDate] = datetime.datetime.now()#add date to top
+                                colval = "%s%s" % (get_column_letter(ROW1.column + 1),addr.row)
+                                ws[colval] = addrBalance#add balance to row
+                        else:
+                                print("balance is the same as sheet")
 
                 wb.save('Wallet_balances.xlsx')
+                wb.close()
                         
             else:
                  client = Client(api_endpoint)
